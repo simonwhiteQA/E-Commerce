95650a2fdf989891f769e08a17e5415ad9f50d7c
simonwhiteQA <simonwhite17@icloud.com>
2021-09-09 17:01:21 +0100

    updates

--- bin/src/main/java/com/qa/spring/ECommerceProjectApplication.java
+++ bin/src/main/java/com/qa/spring/ECommerceProjectApplication.java
@@ -1,7 +1,6 @@
 package com.qa.spring;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
